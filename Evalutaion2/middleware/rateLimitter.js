7facfd33ecc5202ee946cd1c833cf258a03dbca4
Roshankumar056 <roshankr056@gmail.com>
2026-05-31 14:45:32 +0530

    Evaluation2

--- Evalutaion2/middleware/rateLimitter.js
+++ Evalutaion2/middleware/rateLimitter.js
@@ -1,6 +1,6 @@
 const rateLimit=require("express-rate-limit")
 module.exports=rateLimit({
-    window:15*60*1000,
+    windowMs:15*60*1000,
     max:100,
     message:{
         success:false,
--- Evalutaion2/server.js
+++ Evalutaion2/server.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const connectDB = require("./config/db");
 const logger = require("./middleware/loggerMiddleware");
-const rateLimitter = require("./middleware/rateLimitter");
+const rateLimit= require("./middleware/rateLimitter");
 const errorHandler = require("./middleware/errorMiddleware");
 const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
@@ -11,7 +11,7 @@ const app=express()
 connectDB()-0
 app.use(express.json())
 app.use(logger)
-app.use("/api",rateLimitter)
+app.use("/api",rateLimit)
 app.use("/api/auth",authRoutes)
 app.use("/api/posts",postRoutes)
 app.use(errorHandler)
